10df9e48eee5d80edaf29f6dbac6c2d432e3a8c2
70001259 <hadinatajenta122@gmail.com>
2025-08-21 13:29:07 +0700

    update : update axios instance

--- src/api/api.js
+++ src/api/api.js
@@ -1,28 +1,18 @@
-import axios from "axios";
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL :'https://kanjiapi.dev/v1',
-    timeout : 10000
+  baseURL: import.meta.env.VITE_BASE_URL, 
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token')
-        if(token){
-            config.headers.Authorization =  `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => Promise.reject(error)
-)
-
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      router.push('/login')
-    }
-    return Promise.reject(error)
+  (res) => res,
+  (err) => {
+    console.error('API Error:', err)
+    return Promise.reject(err)
   }
 )
 
